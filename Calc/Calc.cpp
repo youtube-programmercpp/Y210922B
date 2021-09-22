20e92b7cd83d4c5973b87a849c33ea9fd69d9b6e
youtube-programmercpp <sbl908k@yahoo.co.jp>
2021-09-23 07:38:27 +0900

    プロジェクト ファイルを追加します。

---
+++ Calc/Calc.cpp
@@ -0,0 +1,58 @@
+﻿// Calc.cpp : アプリケーションのエントリ ポイントを定義します。
+#include <Windows.h>
+#include "resource.h"
+
+#include <string>
+
+std::wstring s;//外部変数…このようなやり方は駄目
+
+INT_PTR CALLBACK DialogProc
+( HWND   hDlg   
+, UINT   message
+, WPARAM wParam 
+, LPARAM lParam 
+)
+{
+	switch (message) {
+	case WM_INITDIALOG:
+		return true;
+	case WM_COMMAND:
+		switch (LOWORD(wParam)) {
+		case IDCANCEL:
+			EndDialog(hDlg, IDCANCEL);
+			return true;
+		case IDC_BUTTON_Num_0: s.append(1, L'0'); SetDlgItemTextW(hDlg, IDC_EDIT1, s.c_str()); return true;
+		case IDC_BUTTON_Num_1: s.append(1, L'1'); SetDlgItemTextW(hDlg, IDC_EDIT1, s.c_str()); return true;
+		case IDC_BUTTON_Num_2: s.append(1, L'2'); SetDlgItemTextW(hDlg, IDC_EDIT1, s.c_str()); return true;
+		case IDC_BUTTON_Num_3: s.append(1, L'3'); SetDlgItemTextW(hDlg, IDC_EDIT1, s.c_str()); return true;
+		case IDC_BUTTON_Num_4: s.append(1, L'4'); SetDlgItemTextW(hDlg, IDC_EDIT1, s.c_str()); return true;
+		case IDC_BUTTON_Num_5: s.append(1, L'5'); SetDlgItemTextW(hDlg, IDC_EDIT1, s.c_str()); return true;
+		case IDC_BUTTON_Num_6: s.append(1, L'6'); SetDlgItemTextW(hDlg, IDC_EDIT1, s.c_str()); return true;
+		case IDC_BUTTON_Num_7: s.append(1, L'7'); SetDlgItemTextW(hDlg, IDC_EDIT1, s.c_str()); return true;
+		case IDC_BUTTON_Num_8: s.append(1, L'8'); SetDlgItemTextW(hDlg, IDC_EDIT1, s.c_str()); return true;
+		case IDC_BUTTON_Num_9: s.append(1, L'9'); SetDlgItemTextW(hDlg, IDC_EDIT1, s.c_str()); return true;
+		default:
+			return false;
+		}
+	default:
+		return false;
+	}
+}
+
+
+int APIENTRY wWinMain
+( _In_     HINSTANCE hInstance    //インスタンスハンドル（モジュールハンドル）
+, _In_opt_ HINSTANCE hPrevInstance//必ず nullptr になる
+, _In_     LPWSTR    lpCmdLine    
+, _In_     int       nCmdShow     
+)
+{
+	DialogBoxParamW
+	( /*_In_opt_ HINSTANCE hInstance     */hInstance     
+	, /*_In_     LPCWSTR   lpTemplateName*/MAKEINTRESOURCEW(IDD_DIALOG_Calc)
+	, /*_In_opt_ HWND      hWndParent    */nullptr
+	, /*_In_opt_ DLGPROC   lpDialogFunc  */DialogProc
+	, /*_In_     LPARAM    dwInitParam   */0
+	);
+	return 0;//0は正常終了、0 以外はエラー
+}
---
+++ Calc/resource.h
@@ -0,0 +1,40 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// Calc.rc で使用
+//
+#define IDC_MYICON                      2
+#define IDD_Y210922CALC_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG_Calc                 130
+#define IDC_BUTTON_Num_7                1004
+#define IDC_EDIT1                       1005
+#define IDC_BUTTON_Num_8                1006
+#define IDC_BUTTON_Num_9                1007
+#define IDC_BUTTON_Num_4                1008
+#define IDC_BUTTON_Num_5                1009
+#define IDC_BUTTON_Num_6                1010
+#define IDC_BUTTON_Num_1                1011
+#define IDC_BUTTON_Num_2                1012
+#define IDC_BUTTON_Num_3                1013
+#define IDC_BUTTON_Sign                 1014
+#define IDC_BUTTON_Num_0                1015
+#define IDC_BUTTON_Dot                  1016
+#define IDC_BUTTON_OpMul                1017
+#define IDC_BUTTON_OpSub                1018
+#define IDC_BUTTON_OpAdd                1019
+#define IDC_BUTTON_OpEqu                1020
+#define IDC_BUTTON_Clear                1021
+#define IDC_BUTTON_OpDiv                1022
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
